55ab634fa1b2bac6e8fb5d6b434d290fc154435e
sdiene696 <sdiene696@gmail.com>
2025-01-17 20:40:52 +0000

    premier commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const card=document.querySelector('.card')
+const labelnom=document.querySelector('.form-labelnom')
+const labelprenom=document.querySelector('.form-labelprenom')
+const labeladresse=document.querySelector('.form-labeladresse')
 const cardSecond=document.querySelector('#cardsecond')
 const btnAjouter=document.querySelector('#btn')
 
@@ -12,7 +15,7 @@ function erreur(){
   //  nom.classList.add("is-invalid")
   //  prenom.classList.add("is-invalid")
   //  adresse.classList.add("is-invalid")
-   btnAjouter.disabled=true;
+  //  btnAjouter.disabled=true;
    
   }else{
     
@@ -34,14 +37,19 @@ nom.addEventListener('input',function(e){
 e.preventDefault()
 let longueurnom=nom.value.length
 if(longueurnom<5){
-  
-   nom.style.borderColor='green'
+   labelnom.style.color='red'
+   nom.style.borderColor='red'
+   nom.classList.add("is-invalid")
 }
 if(longueurnom>=5){
+  labelnom.style.color='yellow'
    nom.style.borderColor='yellow'
+   nom.classList.remove("is-invalid")
 }
 if(longueurnom>=10){
-  nom.style.borderColor='red'
+  labelnom.style.color='green'
+  nom.style.borderColor='green'
+  nom.classList.add("is-valid")
 }
 
 })
@@ -51,28 +59,38 @@ prenom.addEventListener('input',function(e){
 e.preventDefault()
 let longueurprenom=prenom.value.length
 if(longueurprenom<5){
-  
-   prenom.style.borderColor='green'
+   labelprenom.style.color='red'
+   prenom.style.borderColor='red'
+   prenom.classList.add("is-invalid")
 }
 if(longueurprenom>=5){
+  labelprenom.style.color='yellow'
    prenom.style.borderColor='yellow'
+   prenom.classList.remove("is-invalid")
 }
 if(longueurprenom>=10){
-  prenom.style.borderColor='red'
+  labelprenom.style.color='green'
+  prenom.style.borderColor='green'
+  prenom.classList.add("is-valid")
 }
 })
 
 adresse.addEventListener('input',function(e){
   let longueuradresse=adresse.value.length
   if(longueuradresse<5){
-    
-     adresse.style.borderColor='green'
+     labeladresse.style.color='red'
+     adresse.style.borderColor='red'
+     adresse.classList.add("is-invalid")
   }
   if(longueuradresse>=5){
+    labeladresse.style.color='yellow'
      adresse.style.borderColor='yellow'
+     adresse.classList.remove("is-invalid")
   }
   if(longueuradresse>=10){
-    adresse.style.borderColor='red'
+    labeladresse.style.color='green'
+    adresse.style.borderColor='green'
+    adresse.classList.add("is-valid")
   }
 })
 
